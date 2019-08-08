7a161494cc52a6ecab27332966b1406b411ba1f3
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-08 17:20:34 -0400

    creates variables for status arrays, funcs: numberOfSections, tableView for row count, tableView for headers, tableView for cell display

--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -8,13 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    var notStartedArray = Task.allTasks.filter {$0.status == .notStarted}
+    var inProgressArray = Task.allTasks.filter {$0.status == .inProgress}
+    var completedArray = Task.allTasks.filter {$0.status == .completed}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch section {
+        case 0:
+            return notStartedArray.count
+        case 1:
+            return inProgressArray.count
+        case 2:
+            return completedArray.count
+        default:
+            return -1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Not Started"
+        case 1:
+            return "In Progress"
+        case 2:
+            return "Completed"
+        default:
+            return "This shouldn't happen"
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Task Cell") ?? UITableViewCell()
+        
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = notStartedArray[indexPath.row].name
+            cell.detailTextLabel?.text = Task.allTasks[indexPath.row].dueDate.description
+        case 1:
+            cell.textLabel?.text = inProgressArray[indexPath.row].name
+            cell.detailTextLabel?.text = Task.allTasks[indexPath.row].dueDate.description
+        case 2:
+            cell.textLabel?.text = completedArray[indexPath.row].name
+            cell.detailTextLabel?.text = Task.allTasks[indexPath.row].dueDate.description
+        default:
+            fatalError()
+            
+        }
+
+        return cell
+    }
 
 }
 
