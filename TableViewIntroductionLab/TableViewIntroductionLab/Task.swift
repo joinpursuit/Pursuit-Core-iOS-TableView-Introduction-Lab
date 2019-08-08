7f0842b209d9fe44db713efd43422dd8da794062
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-08-08 16:35:31 -0400

    25 minutes have passed, time to commit

--- TableViewIntroductionLab/TableViewIntroductionLab/Task.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/Task.swift
@@ -13,7 +13,7 @@ struct Task {
     
     static var allTasks: [Task] {
         let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en")
+        dateFormatter.locale = Locale(identifier: "en_US")
         dateFormatter.dateFormat = "MM-dd-yyyy"
         return [
             Task(name: "Create project roadmap",
--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -8,10 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var tasks = Task.allTasks
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let date = tasks[indexPath.row].dueDate
+        let name = tasks[indexPath.row].name
+        
+        let myTable = tableView.dequeueReusableCell(withIdentifier: "TaskName")!
+        myTable.textLabel?.text = ("\(name)")
+         myTable.detailTextLabel?.text = ("\(date)")
+        
+        return myTable
+    }
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       // tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
 
