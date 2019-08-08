e6de665056cb1daac13dcf174d7b230330a9471a
Angela Garrovillas <angelagarrovillas@Angelas-MacBook-Pro.local>
2019-08-08 15:23:41 -0400

    completes part1 of Lab

--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -11,25 +11,45 @@ import UIKit
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Task.allTasks.count
+        switch section {
+        case 0:
+            return Task.allTasks.count
+        case 1:
+            return Task.allTasks.count
+        default:
+            return Task.allTasks.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellOne")
         let rowsToSetup = indexPath.row
         let taskInRow = Task.allTasks[rowsToSetup]
-        cell.
+        if let unwrapCell = cell {
+            unwrapCell.textLabel?.text = "\(taskInRow.name)"
+            unwrapCell.detailTextLabel?.text = "\(taskInRow.dueDate.description(with: .autoupdatingCurrent))"
+            return unwrapCell
+        }
+        return UITableViewCell()
+        //cell.textLabel?.text = "\(taskInRow.name)\n\(taskInRow.dueDate.description(with: .autoupdatingCurrent))"
+       // cell.textLabel?.numberOfLines = 2
+        //cell.textLabel?.text = taskInRow.name
+        //cell.detailTextLabel?.text = taskInRow.dueDate.description
+        
     }
     
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.dataSource = self
+        
     }
-
+    
 
 }
 
