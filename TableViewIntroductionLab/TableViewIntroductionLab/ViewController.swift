3498d81af87bab0c9a62fd11f397ce80f769323d
aglegaspi <lex83nyc@gmail.com>
2019-08-08 16:48:34 -0400

    updated the viewDidLoad with dataSource and delegate

--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Task.allTasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = "hello"
+        
+        return cell
+    }
+    
+    @IBOutlet weak var taskTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        taskTableView.dataSource = self
+        taskTableView.delegate = self
     }
 
 
