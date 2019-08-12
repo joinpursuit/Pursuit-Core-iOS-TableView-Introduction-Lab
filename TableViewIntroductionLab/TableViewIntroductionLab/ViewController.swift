9dea576696c7b9576a004ff3a5f4dd0cb558c6a4
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-08-12 10:09:53 -0400

    updating

--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController, UITableViewDataSource {
     @IBAction func sortOrderButton(_ sender: UIButton) {
         
     }
-
     
 //    func isEnabledButton(){
 //        for _ in sortOrderButton{
